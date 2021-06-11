90bd0ee6815d0e69b1b8abe86d695c6ff5774b67
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-06-11 16:45:25 +0200

    rtb: Add button support

--- rtb_builder/rtb_builder.c
+++ rtb_builder/rtb_builder.c
@@ -3,10 +3,21 @@
 /*      INCLUDES                                                    */
 
 // C STANDARD
-#include <stdbool.h>    // bool
+#include <stdbool.h>        // bool
+#include <stdint.h>         // uint*_t
+
+// NRF
+#include "boards.h"         // bsp_board_led_*
+#include "nrf_log.h"        // NRF_LOG_INFO
+#include "sdk_errors.h"     // ret_code_t
+
+// NRF APPS
+#include "app_button.h"     // app_button_*
+#include "app_error.h"      // APP_ERROR_CHECK
+#include "app_timer.h"      // APP_TIMER_TICKS
 
 // LUOS
-#include "luos.h"       // Luos_CreateContainer, container_t
+#include "luos.h"           // Luos_CreateContainer, container_t
 
 /*      STATIC VARIABLES & CONSTANTS                                */
 
@@ -20,19 +31,67 @@ static  container_t*    s_rtb_builder       = NULL;
 // Describes if a detection was asked.
 static  bool            s_detection_asked   = false;
 
+// Button to press.
+#define                 BUTTON_IDX  BSP_BUTTON_0
+
+// Button detection delay.
+static const uint32_t BTN_DTX_DELAY = APP_TIMER_TICKS(50);
+
 /*      STATIC FUNCTIONS                                            */
+
+// Initializes the button functionality.
+static void init_button(void);
+
+/*      CALLBACKS                                                   */
 
 // Does nothing: only commands are performed through button IT.
 static void RTBBuilder_MsgHandler(container_t* container, msg_t* msg);
 
+// If the index is the defined one, toggles the detection boolean.
+static void button_evt_handler(uint8_t btn_idx, uint8_t event);
+
 void RTBBuilder_Init(void)
 {
+    init_button();
+
     revision_t revision = { .unmap = REV };
     s_rtb_builder = Luos_CreateContainer(NULL, RTB_TYPE, RTB_ALIAS,
                                          revision);
+
+    app_button_enable();
 }
 
 void RTBBuilder_Loop(void)
 {
     // if detection asked, run detection.
+}
+
+static void init_button(void)
+{
+    static app_button_cfg_t buttons[] =
+    {
+        {
+            BUTTON_IDX,
+            APP_BUTTON_ACTIVE_LOW,
+            BUTTON_PULL,
+            button_evt_handler,
+        },
+    };
+
+    ret_code_t err_code = app_button_init(buttons, 1, BTN_DTX_DELAY);
+    APP_ERROR_CHECK(err_code);
+}
+
+static void button_evt_handler(uint8_t btn_idx, uint8_t event)
+{
+    #ifdef DEBUG
+    NRF_LOG_INFO("Button event %u on btn %u!", event, btn_idx);
+    #endif /* DEBUG */
+
+    if (btn_idx != BUTTON_IDX)
+    {
+        return;
+    }
+
+    s_detection_asked = (event == APP_BUTTON_PUSH);
 }
--- rtb_builder/rtb_builder.cmake
+++ rtb_builder/rtb_builder.cmake
@@ -9,8 +9,33 @@ add_library( rtb_builder
     "${RTB_BUILDER_PATH}/rtb_builder.c"
 )
 
+add_compile_definitions(
+    BSP_DEFINES_ONLY
+    CONFIG_GPIO_AS_PINRESET
+    DEBUG
+)
+
 target_link_libraries( rtb_builder PUBLIC
+    #Luos
     luos
+
+    # NRF
+    # External
+    nrf5_ext_fprintf
+    nrf5_ext_segger_rtt
+    # Logger
+    nrf5_log
+    nrf5_log_backend_serial
+    nrf5_log_backend_rtt
+    nrf5_log_default_backends
+    # Drivers
+    nrf5_nrfx_gpiote
+    nrf5_nrfx_prs
+    # Application
+    nrf5_app_error
+    nrf5_app_util_platform
+    nrf5_app_timer
+    nrf5_app_button
 )
 
 target_include_directories( rtb_builder PUBLIC
