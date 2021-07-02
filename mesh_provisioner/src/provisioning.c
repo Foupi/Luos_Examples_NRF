260f0bfa19a224980954182a9dc65c52f4195a47
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-07-02 18:58:30 +0200

    provisioning: Add persistent data fetching and mesh stack activation

--- mesh_provisioner/src/provisioning.c
+++ mesh_provisioner/src/provisioning.c
@@ -56,8 +56,12 @@ static nrf_mesh_prov_bearer_adv_t   s_prov_bearer;
 static const uint16_t               PROV_ELM_ADDRESS        = 0x0001;
 
 // Key indexes.
-static const uint16_t               PROV_NETKEY_IDX         = 0x0000;
-static const uint16_t               PROV_APPKEY_IDX         = 0x0000;
+static const mesh_key_index_t       PROV_NETKEY_IDX         = 0x0000;
+static const mesh_key_index_t       PROV_APPKEY_IDX         = 0x0000;
+
+// Amounts of expected key indexes when fetching from persistent data.
+static const uint32_t               NB_NETKEY_IDX           = 1;
+static const uint32_t               NB_APPKEY_IDX           = 1;
 
 static struct
 {
@@ -80,6 +84,16 @@ static void ble_stack_init(void);
 
 // Registers the BLE callback in the BLE stack.
 static void ble_observers_register(void);
+
+/* Generates keys and handles, stores them in the DSM and in the static
+** context.
+*/
+static void network_ctx_generate(void);
+
+/* Fetches the handles stored in the DSM and keeps them in the static
+** context.
+*/
+static void network_ctx_fetch(void);
 
 /*      CALLBACKS                                                   */
 
@@ -135,78 +149,34 @@ void provisioning_init(void)
                                   &oob_caps, mesh_prov_event_cb);
     APP_ERROR_CHECK(err_code);
 
-    NRF_LOG_INFO("Provisioning context initialized!");
-
     prov_bearer_t* generic_bearer = nrf_mesh_prov_bearer_adv_interface_get(&s_prov_bearer);
     err_code = nrf_mesh_prov_bearer_add(&s_prov_ctx, generic_bearer);
     APP_ERROR_CHECK(err_code);
-
-    NRF_LOG_INFO("Provisioning bearer added to provisioning context!");
 }
 
 void network_ctx_init(void)
 {
-    ret_code_t                  err_code;
-    dsm_local_unicast_address_t local_addr_range;
-
     if (s_device_provisioned)
     {
-        NRF_LOG_INFO("Fetching handles from persistent data!");
-
-        // FIXME Retrieve local address range.
-
-        // FIXME Retrieve netkey index, then retrieve and store handle.
-
-        // FIXME Retrieve appkey index, then retrieve and store handle.
-
-        // FIXME Retrieve and store self devkey handle.
+        NRF_LOG_INFO("Fetching network context from persistent storage!");
+        network_ctx_fetch();
     }
     else
     {
         NRF_LOG_INFO("Generating network context!");
-
-        memset(&local_addr_range, 0,
-               sizeof(dsm_local_unicast_address_t));
-        local_addr_range.address_start  = PROV_ELM_ADDRESS;
-        local_addr_range.count          = ACCESS_ELEMENT_COUNT;
-
-        err_code = dsm_local_unicast_addresses_set(&local_addr_range);
-        APP_ERROR_CHECK(err_code);
-
-        NRF_LOG_INFO("Local address range set!");
-
-        rand_hw_rng_get(s_network_ctx.netkey, NRF_MESH_KEY_SIZE);
-        err_code = dsm_subnet_add(PROV_NETKEY_IDX, s_network_ctx.netkey,
-                                  &(s_network_ctx.netkey_handle));
-        APP_ERROR_CHECK(err_code);
-
-        rand_hw_rng_get(s_network_ctx.appkey, NRF_MESH_KEY_SIZE);
-        err_code = dsm_appkey_add(PROV_APPKEY_IDX,
-                                  s_network_ctx.netkey_handle,
-                                  s_network_ctx.appkey,
-                                  &(s_network_ctx.appkey_handle));
-        APP_ERROR_CHECK(err_code);
-
-        rand_hw_rng_get(s_network_ctx.self_devkey, NRF_MESH_KEY_SIZE);
-        err_code = dsm_devkey_add(PROV_ELM_ADDRESS,
-                                  s_network_ctx.netkey_handle,
-                                  s_network_ctx.self_devkey,
-                                  &(s_network_ctx.self_devkey_handle));
-        APP_ERROR_CHECK(err_code);
-
-        // FIXME Bind self devkey to config server.
-        err_code = config_server_bind(s_network_ctx.self_devkey_handle);
-        APP_ERROR_CHECK(err_code);
-
-        NRF_LOG_INFO("Network context generated!");
+        network_ctx_generate();
     }
+
+    NRF_LOG_INFO("Netkey handle: 0x%x; Appkey handle: 0x%x; Self devkey handle: 0x%x!",
+                 s_network_ctx.netkey_handle,
+                 s_network_ctx.appkey_handle,
+                 s_network_ctx.self_devkey_handle);
 }
 
 void mesh_start(void)
 {
-    // FIXME Call to `mesh_stack_start`.
-
-    NRF_LOG_INFO("Starting Mesh stack!");
+    ret_code_t err_code = mesh_stack_start();
+    APP_ERROR_CHECK(err_code);
 }
 
 void prov_scan_start(void)
@@ -247,6 +217,93 @@ static void ble_observers_register(void)
                          NRF_SDH_BLE_STACK_OBSERVER_PRIO,
                          sd_soc_event_cb,
                          NULL);
+}
+
+static void network_ctx_generate(void)
+{
+    ret_code_t                  err_code;
+
+    dsm_local_unicast_address_t local_addr_range;
+    memset(&local_addr_range, 0, sizeof(dsm_local_unicast_address_t));
+    local_addr_range.address_start  = PROV_ELM_ADDRESS;
+    local_addr_range.count          = ACCESS_ELEMENT_COUNT;
+
+    err_code = dsm_local_unicast_addresses_set(&local_addr_range);
+    APP_ERROR_CHECK(err_code);
+
+    rand_hw_rng_get(s_network_ctx.netkey, NRF_MESH_KEY_SIZE);
+    err_code = dsm_subnet_add(PROV_NETKEY_IDX, s_network_ctx.netkey,
+                              &(s_network_ctx.netkey_handle));
+    APP_ERROR_CHECK(err_code);
+
+    rand_hw_rng_get(s_network_ctx.appkey, NRF_MESH_KEY_SIZE);
+    err_code = dsm_appkey_add(PROV_APPKEY_IDX,
+                              s_network_ctx.netkey_handle,
+                              s_network_ctx.appkey,
+                              &(s_network_ctx.appkey_handle));
+    APP_ERROR_CHECK(err_code);
+
+    rand_hw_rng_get(s_network_ctx.self_devkey, NRF_MESH_KEY_SIZE);
+    err_code = dsm_devkey_add(PROV_ELM_ADDRESS,
+                              s_network_ctx.netkey_handle,
+                              s_network_ctx.self_devkey,
+                              &(s_network_ctx.self_devkey_handle));
+    APP_ERROR_CHECK(err_code);
+
+    err_code = config_server_bind(s_network_ctx.self_devkey_handle);
+    APP_ERROR_CHECK(err_code);
+}
+
+static void network_ctx_fetch(void)
+{
+    ret_code_t                  err_code;
+    dsm_local_unicast_address_t local_addr_range;
+    dsm_local_unicast_addresses_get(&local_addr_range);
+
+    // Temporary variables to ease writing.
+    uint32_t                    nb_indexes;
+    mesh_key_index_t            key_index_buffer;
+    dsm_handle_t                curr_handle;
+
+    // FIXME Retrieve netkey index, then retrieve and store handle.
+    err_code = dsm_subnet_get_all(&key_index_buffer, &nb_indexes);
+    APP_ERROR_CHECK(err_code);
+    if (nb_indexes != NB_NETKEY_IDX)
+    {
+        // FIXME Manage error.
+    }
+
+    curr_handle = dsm_net_key_index_to_subnet_handle(key_index_buffer);
+    if (curr_handle == DSM_HANDLE_INVALID)
+    {
+        // FIXME Manage error.
+    }
+    s_network_ctx.netkey_handle = curr_handle;
+
+    err_code = dsm_appkey_get_all(s_network_ctx.netkey_handle,
+                                  &key_index_buffer, &nb_indexes);
+    APP_ERROR_CHECK(err_code);
+    if (nb_indexes != NB_APPKEY_IDX)
+    {
+        // FIXME Manage error.
+    }
+
+    curr_handle = dsm_appkey_index_to_appkey_handle(key_index_buffer);
+    if (curr_handle == DSM_HANDLE_INVALID)
+    {
+        // FIXME Manage error.
+    }
+    s_network_ctx.appkey_handle = curr_handle;
+
+    // FIXME Retrieve and store self devkey handle.
+    err_code = dsm_devkey_handle_get(local_addr_range.address_start,
+                                     &curr_handle);
+    APP_ERROR_CHECK(err_code);
+    if (curr_handle == DSM_HANDLE_INVALID)
+    {
+        // FIXME Manage error.
+    }
+    s_network_ctx.self_devkey_handle = curr_handle;
 }
 
 static void sd_soc_event_cb(uint32_t event, void* context)
