2135f880b257f507284b3a1989af9abfdc3b29a8
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-08-05 14:51:56 +0200

    mesh bridge: Add state management to RTB model

--- mesh_bridge/src/app_luos_rtb_model.c
+++ mesh_bridge/src/app_luos_rtb_model.c
@@ -15,12 +15,37 @@
 #include <string.h>                 // memset
 
 // LUOS
-#include "luos_list.h"              // FIXME VOID_MOD
 #include "routing_table.h"          // routing_table_t
 
 // CUSTOM
 #include "luos_rtb_model.h"         // luos_rtb_model_*
 #include "luos_rtb_model_common.h"  // LUOS_RTB_MODEL_MAX_RTB_ENTRY
+
+/*      TYPEDEFS                                                    */
+
+// Luos RTB model management states.
+typedef enum
+{
+    // Idle state.
+    LUOS_RTB_MODEL_STATE_IDLE,
+
+    // Node initiating procedure:
+
+    // Getting remote RTB.
+    LUOS_RTB_MODEL_STATE_GETTING,
+
+    // Publishing local RTB.
+    LUOS_RTB_MODEL_STATE_PUBLISHING,
+
+    // Node responding to the procedure:
+
+    // Replying to request with local RTB.
+    LUOS_RTB_MODEL_STATE_REPLYING,
+
+    // Receiving remote RTB.
+    LUOS_RTB_MODEL_STATE_RECEIVING,
+
+} luos_rtb_model_state_t;
 
 /*      STATIC VARIABLES & CONSTANTS                                */
 
@@ -37,6 +62,13 @@ static const uint32_t   WAIT_FIRST_ENTRY_DELAY_TICKS    = APP_TIMER_TICKS(WAIT_F
 // Delay to wait for next RTB entry.
 #define                 WAIT_NEXT_ENTRY_DELAY_MS        500
 static const uint32_t   WAIT_NEXT_ENTRY_DELAY_TICKS     = APP_TIMER_TICKS(WAIT_NEXT_ENTRY_DELAY_MS);
+
+static struct
+{
+    // Current state
+    luos_rtb_model_state_t  curr_state;
+
+}                       s_luos_rtb_model_ctx;
 
 /*      STATIC FUNCTIONS                                            */
 
@@ -73,6 +105,8 @@ void app_luos_rtb_model_init(void)
                                 APP_TIMER_MODE_SINGLE_SHOT,
                                 entries_reception_timeout_cb);
     APP_ERROR_CHECK(err_code);
+
+    s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_IDLE;
 }
 
 void app_luos_rtb_model_address_set(uint16_t device_address)
@@ -82,6 +116,12 @@ void app_luos_rtb_model_address_set(uint16_t device_address)
 
 void app_luos_rtb_model_get(void)
 {
+    if (s_luos_rtb_model_ctx.curr_state != LUOS_RTB_MODEL_STATE_IDLE)
+    {
+        NRF_LOG_INFO("Cannot send Luos RTB GET requests out of IDLE mode!");
+        return;
+    }
+
     ret_code_t err_code;
 
     luos_rtb_model_get(&s_luos_rtb_model);
@@ -89,11 +129,9 @@ void app_luos_rtb_model_get(void)
     err_code = app_timer_start(s_entries_reception_timer,
                                WAIT_FIRST_ENTRY_DELAY_TICKS, NULL);
     APP_ERROR_CHECK(err_code);
-}
 
-static void rtb_model_get_cb(void)
-{
-    NRF_LOG_INFO("Luos RTB GET request received!");
+    NRF_LOG_INFO("Engaging ext-RTB procedure: switch to GETTING state!");
+    s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_GETTING;
 }
 
 static bool get_rtb_entries(routing_table_t* rtb_entries,
@@ -129,13 +167,45 @@ static bool get_rtb_entries(routing_table_t* rtb_entries,
     }
 
     *nb_entries                             = returned_nb_entries;
+
+    // FIXME Create callback for end of RTB entries enqueuing?
+    if (s_luos_rtb_model_ctx.curr_state == LUOS_RTB_MODEL_STATE_REPLYING)
+    {
+        NRF_LOG_INFO("Switch to RECEIVING mode!");
+        s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_RECEIVING;
+    }
+    else if (s_luos_rtb_model_ctx.curr_state == LUOS_RTB_MODEL_STATE_PUBLISHING)
+    {
+        NRF_LOG_INFO("Ext-RTB procedure complete: switch back to IDLE mode!");
+        s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_IDLE;
+    }
+
     return true;
+}
+
+static void rtb_model_get_cb(void)
+{
+    if (s_luos_rtb_model_ctx.curr_state != LUOS_RTB_MODEL_STATE_IDLE)
+    {
+        NRF_LOG_INFO("Luos RTB GET request received while not in IDLE mode!");
+        return;
+    }
+
+    NRF_LOG_INFO("Luos RTB GET request received: switch to REPLYING mode!");
+    s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_REPLYING;
 }
 
 static void rtb_model_status_cb(uint16_t src_addr,
                                 const routing_table_t* entry,
                                 uint16_t entry_idx)
 {
+    if ((s_luos_rtb_model_ctx.curr_state != LUOS_RTB_MODEL_STATE_GETTING)
+        && (s_luos_rtb_model_ctx.curr_state != LUOS_RTB_MODEL_STATE_RECEIVING))
+    {
+        NRF_LOG_INFO("Luos RTB STATUS message received while not in a reception mode!");
+        return;
+    }
+
     ret_code_t  err_code;
 
     err_code = app_timer_stop(s_entries_reception_timer);
@@ -152,5 +222,16 @@ static void rtb_model_status_cb(uint16_t src_addr,
 
 static void entries_reception_timeout_cb(void* context)
 {
-    NRF_LOG_INFO("Entries reception timeout!");
+    if (s_luos_rtb_model_ctx.curr_state == LUOS_RTB_MODEL_STATE_GETTING)
+    {
+        NRF_LOG_INFO("Reception timeout for remote nodes entries: switch to PUBLISHING mode!");
+        s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_PUBLISHING;
+
+        // FIXME Publish RTB.
+    }
+    else if (s_luos_rtb_model_ctx.curr_state == LUOS_RTB_MODEL_STATE_RECEIVING)
+    {
+        NRF_LOG_INFO("Reception timeout for remote node entries, end of ext-RTB procedure: switch back to IDLE mode!");
+        s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_IDLE;
+    }
 }
