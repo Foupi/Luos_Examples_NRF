0eaa6f541167df943bf514b12c3239a8a3f395ac
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-08-05 14:13:43 +0200

    mesh bridge: Add timer to detect end of entries reception

--- mesh_bridge/src/app_luos_rtb_model.c
+++ mesh_bridge/src/app_luos_rtb_model.c
@@ -4,6 +4,11 @@
 
 // NRF
 #include "nrf_log.h"                // NRF_LOG_INFO
+#include "sdk_errors.h"             // ret_code_t
+
+// NRF APPS
+#include "app_error.h"              // APP_ERROR_CHECK
+#include "app_timer.h"              // app_timer_*
 
 // C STANDARD
 #include <stdint.h>                 // uint16_t
@@ -22,6 +27,17 @@
 // Luos RTB model instance.
 static luos_rtb_model_t s_luos_rtb_model;
 
+// Timer used to detect end of RTB reception.
+APP_TIMER_DEF(s_entries_reception_timer);
+
+// Delay to wait for first RTB entry.
+#define                 WAIT_FIRST_ENTRY_DELAY_MS       1000
+static const uint32_t   WAIT_FIRST_ENTRY_DELAY_TICKS    = APP_TIMER_TICKS(WAIT_FIRST_ENTRY_DELAY_MS);
+
+// Delay to wait for next RTB entry.
+#define                 WAIT_NEXT_ENTRY_DELAY_MS        500
+static const uint32_t   WAIT_NEXT_ENTRY_DELAY_TICKS     = APP_TIMER_TICKS(WAIT_NEXT_ENTRY_DELAY_MS);
+
 /*      STATIC FUNCTIONS                                            */
 
 // FIXME Retrieves local RTB and stores it in the given arguments.
@@ -38,8 +54,13 @@ static void rtb_model_status_cb(uint16_t src_addr,
                                 const routing_table_t* entry,
                                 uint16_t entry_idx);
 
+// FIXME Logs message.
+static void entries_reception_timeout_cb(void* context);
+
 void app_luos_rtb_model_init(void)
 {
+    ret_code_t                      err_code;
+
     luos_rtb_model_init_params_t    init_params;
     memset(&init_params, 0, sizeof(luos_rtb_model_init_params_t));
     init_params.get_cb                      = rtb_model_get_cb;
@@ -47,6 +68,11 @@ void app_luos_rtb_model_init(void)
     init_params.status_cb                   = rtb_model_status_cb;
 
     luos_rtb_model_init(&s_luos_rtb_model, &init_params);
+
+    err_code = app_timer_create(&s_entries_reception_timer,
+                                APP_TIMER_MODE_SINGLE_SHOT,
+                                entries_reception_timeout_cb);
+    APP_ERROR_CHECK(err_code);
 }
 
 void app_luos_rtb_model_address_set(uint16_t device_address)
@@ -56,7 +82,13 @@ void app_luos_rtb_model_address_set(uint16_t device_address)
 
 void app_luos_rtb_model_get(void)
 {
+    ret_code_t err_code;
+
     luos_rtb_model_get(&s_luos_rtb_model);
+
+    err_code = app_timer_start(s_entries_reception_timer,
+                               WAIT_FIRST_ENTRY_DELAY_TICKS, NULL);
+    APP_ERROR_CHECK(err_code);
 }
 
 static void rtb_model_get_cb(void)
@@ -104,7 +136,21 @@ static void rtb_model_status_cb(uint16_t src_addr,
                                 const routing_table_t* entry,
                                 uint16_t entry_idx)
 {
+    ret_code_t  err_code;
+
+    err_code = app_timer_stop(s_entries_reception_timer);
+    APP_ERROR_CHECK(err_code);
+
     NRF_LOG_INFO("Luos RTB STATUS message received from node 0x%x: entry %u has ID 0x%x, type %s, alias %s!",
                  src_addr, entry_idx, entry->id,
                  RoutingTB_StringFromType(entry->type), entry->alias);
+
+    err_code = app_timer_start(s_entries_reception_timer,
+                               WAIT_NEXT_ENTRY_DELAY_TICKS, NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
+static void entries_reception_timeout_cb(void* context)
+{
+    NRF_LOG_INFO("Entries reception timeout!");
 }
