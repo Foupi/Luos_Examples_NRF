bcbae2c3b19301fb9bbc08b8a6831efbf62126df
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-08-06 17:13:49 +0200

    mesh bridge: Add local RTB storing
    This is done to prevent sending to remote devices routing table entries
    for their own containers.

--- mesh_bridge/src/app_luos_rtb_model.c
+++ mesh_bridge/src/app_luos_rtb_model.c
@@ -70,28 +70,46 @@ static const uint32_t   WAIT_NEXT_ENTRY_PUBLISH_DELAY_TICKS = APP_TIMER_TICKS(WA
 
 static struct
 {
-    // Current state
+    // Current state.
     luos_rtb_model_state_t  curr_state;
+
+    // Table of local containers.
+    struct
+    {
+        // Number of local containers.
+        uint16_t        nb_local_containers;
+
+        // Table of entries.
+        routing_table_t local_containers[LUOS_RTB_MODEL_MAX_RTB_ENTRY];
+
+    }                       local_container_table;
 
 }                       s_luos_rtb_model_ctx;
 
 /*      STATIC FUNCTIONS                                            */
 
-// FIXME Retrieves local RTB and stores it in the given arguments.
+// Retrieves local RTB and stores it in the given arguments.
 static bool get_rtb_entries(routing_table_t* rtb_entries,
                             uint16_t* nb_entries);
 
+// Stores routing table inside the static local containers table.
+static bool retrieve_local_rtb(void);
+
 /*      CALLBACKS                                                   */
 
-// FIXME Logs message.
+/* Clears the remote containers table for the given address and switches
+** state to REPLYING.
+*/
 static void rtb_model_get_cb(uint16_t src_addr);
 
-// FIXME Logs message.
+/* Stores the received entry in the remote containers table and restarts
+** the timer.
+*/
 static void rtb_model_status_cb(uint16_t src_addr,
                                 const routing_table_t* entry,
                                 uint16_t entry_idx);
 
-// FIXME Logs message.
+// Switches state depending on the current state.
 static void entries_reception_timeout_cb(void* context);
 
 void app_luos_rtb_model_init(void)
@@ -112,6 +130,8 @@ void app_luos_rtb_model_init(void)
     APP_ERROR_CHECK(err_code);
 
     s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_IDLE;
+    memset(&(s_luos_rtb_model_ctx.local_container_table), 0,
+           sizeof(s_luos_rtb_model_ctx.local_container_table));
 
     remote_container_table_clear();
 }
@@ -148,36 +168,19 @@ static bool get_rtb_entries(routing_table_t* rtb_entries,
 {
     ret_code_t          err_code;
 
-    uint16_t            local_nb_entries    = RoutingTB_GetLastEntry();
-    if (local_nb_entries == 0)
+    if (s_luos_rtb_model_ctx.local_container_table.nb_local_containers == 0)
     {
-        // Local routing table is empty: detection was not performed.
-        return false;
-    }
-
-    routing_table_t*    local_rtb           = RoutingTB_Get();
-    uint16_t            returned_nb_entries = 0;
-    for (uint16_t entry_idx = 0; entry_idx < local_nb_entries;
-         entry_idx++)
-    {
-        if (local_rtb[entry_idx].mode != CONTAINER)
+        // Local routing table has not been stored in static context.
+        bool    detection_complete = retrieve_local_rtb();
+        if (!detection_complete)
         {
-            // Only export containers.
-            continue;
-        }
-
-        memcpy(rtb_entries + returned_nb_entries, local_rtb + entry_idx,
-               sizeof(routing_table_t));
-        returned_nb_entries++;
-
-        if (returned_nb_entries >= LUOS_RTB_MODEL_MAX_RTB_ENTRY)
-        {
-            // Do not export any more entries.
-            break;
+            return false;
         }
     }
 
-    *nb_entries                             = returned_nb_entries;
+    memcpy(rtb_entries, s_luos_rtb_model_ctx.local_container_table.local_containers,
+           s_luos_rtb_model_ctx.local_container_table.nb_local_containers * sizeof(routing_table_t));
+    *nb_entries = s_luos_rtb_model_ctx.local_container_table.nb_local_containers;
 
     // FIXME Create callback for end of RTB entries enqueuing?
     if (s_luos_rtb_model_ctx.curr_state == LUOS_RTB_MODEL_STATE_REPLYING)
@@ -193,6 +196,39 @@ static bool get_rtb_entries(routing_table_t* rtb_entries,
     {
         NRF_LOG_INFO("Published local RTB, ext-RTB procedure complete: switch back to IDLE mode!");
         s_luos_rtb_model_ctx.curr_state = LUOS_RTB_MODEL_STATE_IDLE;
+    }
+
+    return true;
+}
+
+static bool retrieve_local_rtb(void)
+{
+    uint16_t            local_nb_entries    = RoutingTB_GetLastEntry();
+    if (local_nb_entries == 0)
+    {
+        // Local routing table is empty: detection was not performed.
+        return false;
+    }
+
+    routing_table_t*    local_rtb           = RoutingTB_Get();
+    for (uint16_t entry_idx = 0; entry_idx < local_nb_entries;
+         entry_idx++)
+    {
+        if (local_rtb[entry_idx].mode != CONTAINER)
+        {
+            // Only export containers.
+            continue;
+        }
+
+        memcpy(s_luos_rtb_model_ctx.local_container_table.local_containers + s_luos_rtb_model_ctx.local_container_table.nb_local_containers,
+               local_rtb + entry_idx, sizeof(routing_table_t));
+        s_luos_rtb_model_ctx.local_container_table.nb_local_containers++;
+
+        if (s_luos_rtb_model_ctx.local_container_table.nb_local_containers >= LUOS_RTB_MODEL_MAX_RTB_ENTRY)
+        {
+            // Do not export any more entries.
+            break;
+        }
     }
 
     return true;
@@ -237,6 +273,7 @@ static void rtb_model_status_cb(uint16_t src_addr,
     if (!insertion_complete)
     {
         NRF_LOG_INFO("Remote container table full: could not insert received entry!");
+        // FIXME Change state to stop receiving unstorable entries?
     }
 
     if (s_luos_rtb_model_ctx.curr_state == LUOS_RTB_MODEL_STATE_GETTING)
