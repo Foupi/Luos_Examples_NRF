59293f5f3bd5e813c3f0f8d22c585d6a6777f83c
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-08-09 16:21:40 +0200

    provisioner: Switch behaviour control from bool toggle to Luos msg

--- mesh_provisioner/include/mesh_provisioner.h
+++ mesh_provisioner/include/mesh_provisioner.h
@@ -11,10 +11,8 @@
 
 /*      DEFINES                                                     */
 
-// Type of the Mesh Provisioner container.
-#define MESH_PROVISIONER_TYPE   VOID_MOD
-
-// Default alias of the Mesh Provisioner container.
+// Type and default alias for the Mesh Provisioner container.
+#define MESH_PROVISIONER_TYPE   STATE_MOD
 #define MESH_PROVISIONER_ALIAS  "mesh_prov"
 
 /* Initializes and starts the Mesh stack, and initializes the low-level
@@ -24,8 +22,5 @@ void MeshProvisioner_Init(void);
 
 // Enables or disables provisioning depending on state and user request.
 void MeshProvisioner_Loop(void);
-
-// Describes the expected provisioning scan state.
-extern bool g_prov_scan_req;
 
 #endif /* ! MESH_PROVISIONER_H */
--- mesh_provisioner/src/mesh_provisioner.c
+++ mesh_provisioner/src/mesh_provisioner.c
@@ -25,12 +25,6 @@
 #define REV {0,0,1}
 #endif
 
-// Describes if scanning was asked by the user.
-bool                g_prov_scan_req         = false;
-
-// Describes if the container is currently scanning for devices.
-static bool         s_prov_scanning_state   = false;
-
 /*      CALLBACKS                                                   */
 
 // Does nothing as changes are triggered by a boolean toggle.
@@ -40,27 +34,15 @@ static void MeshProvisioner_MsgHandler(container_t* container,
 void MeshProvisioner_Init(void)
 {
     mesh_init();
-
-    NRF_LOG_INFO("Mesh stack initialized!");
-
     provisioning_init();
-
-    NRF_LOG_INFO("Provisioning module initialized!");
-
     network_ctx_init();
-
     NRF_LOG_INFO("Netkey handle: 0x%x; Appkey handle: 0x%x; Self devkey handle: 0x%x!",
                  g_network_ctx.netkey_handle,
                  g_network_ctx.appkey_handle,
                  g_network_ctx.self_devkey_handle);
 
     prov_conf_init();
-
-    NRF_LOG_INFO("Provisioning configuration initialized!");
-
     mesh_start();
-
-    NRF_LOG_INFO("Mesh stack started!");
 
     revision_t revision = { .unmap = REV };
 
@@ -71,14 +53,18 @@ void MeshProvisioner_Init(void)
 }
 
 void MeshProvisioner_Loop(void)
-{
-    if (g_prov_scan_req != s_prov_scanning_state)
-    {
-        /* Difference between these variables means a command was
-        ** received.
-        */
+{}
 
-        if (g_prov_scan_req)
+static void MeshProvisioner_MsgHandler(container_t* container,
+                                       msg_t* msg)
+{
+    switch (msg->header.cmd)
+    {
+    case IO_STATE:
+    {
+        bool req = msg->data[0];
+
+        if (req)
         {
             prov_scan_start();
         }
@@ -86,12 +72,10 @@ void MeshProvisioner_Loop(void)
         {
             prov_scan_stop();
         }
-
-        s_prov_scanning_state = g_prov_scan_req;
     }
-}
+        break;
 
-static void MeshProvisioner_MsgHandler(container_t* container,
-                                       msg_t* msg)
-{
+    default:
+        break;
+    }
 }
