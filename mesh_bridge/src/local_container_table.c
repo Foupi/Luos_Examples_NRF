7484730bbfd992284268c88c040a86454c3cf522
Pierre Foucart <pierre.foucart@elsys-design.com>
2021-08-11 14:21:04 +0200

    mesh bridge: Implement (clean) local RTB retrieval

--- mesh_bridge/src/local_container_table.c
+++ mesh_bridge/src/local_container_table.c
@@ -5,11 +5,14 @@
 // C STANDARD
 #include <stdbool.h>                // bool
 #include <stdint.h>                 // uint16_t
+#include <string.h>                 // memcpy
 
 // LUOS
+#include "config.h"                 // MAX_ALIAS_SIZE
 #include "routing_table.h"          // routing_table_t
 
 // CUSTOM
+#include "mesh_bridge.h"            // MESH_BRIDGE_MOD
 #include "luos_rtb_model_common.h"  // LUOS_RTB_MODEL_MAX_RTB_ENTRY
 
 /*      STATIC VARIABLES & CONSTANTS                                */
@@ -25,10 +28,58 @@ static struct
 
 } s_local_container_table   = { 0 };
 
+/*      STATIC FUNCTIONS                                            */
+
+// Returns true if the entry must be stored in the local table.
+static bool is_entry_to_store(routing_table_t* entry);
+
 uint16_t local_container_table_fill(void)
 {
-    // FIXME
-    return 0;
+    routing_table_t*    local_rtb           = RoutingTB_Get();
+    uint16_t            nb_local_entries    = RoutingTB_GetLastEntry();
+
+    if (nb_local_entries == 0)
+    {
+        // Detection was not performed yet.
+        return 0;
+    }
+
+    for (uint16_t entry_idx = 0; entry_idx < nb_local_entries;
+         entry_idx++)
+    {
+        routing_table_t*    entry       = local_rtb + entry_idx;
+
+        if (entry->mode != CONTAINER)
+        {
+            // Only export container entries.
+            continue;
+        }
+
+        bool must_store = is_entry_to_store(entry);
+        if (!must_store)
+        {
+            continue;
+        }
+
+        local_container_t*  local_entry;
+        local_entry = s_local_container_table.local_containers + s_local_container_table.nb_local_containers;
+
+        local_entry->local_id           = entry->id;
+        local_entry->exposed_entry.mode = CONTAINER;
+        local_entry->exposed_entry.id   = s_local_container_table.nb_local_containers;
+        local_entry->exposed_entry.type = entry->type;
+        memcpy(local_entry->exposed_entry.alias, entry->alias,
+               MAX_ALIAS_SIZE * sizeof(char));
+
+        s_local_container_table.nb_local_containers++;
+        if (s_local_container_table.nb_local_containers >= LUOS_RTB_MODEL_MAX_RTB_ENTRY)
+        {
+            // Do not store more RTB entries.
+            break;
+        }
+    }
+
+    return s_local_container_table.nb_local_containers;
 }
 
 uint16_t local_container_table_get_nb_entries(void)
@@ -51,4 +102,21 @@ routing_table_t* local_container_table_get_entry(uint16_t entry_idx)
 void local_container_table_update_local_ids(void)
 {
     // FIXME for loop
+}
+
+static bool is_entry_to_store(routing_table_t* entry)
+{
+    /* This format might not be the shortest but adding conditions is
+    ** easier this way...
+    */
+
+    if (entry->type == MESH_BRIDGE_MOD)
+    {
+        // Do not export Mesh Bridge container.
+        return false;
+    }
+
+    // Add conditions.
+
+    return true;
 }
